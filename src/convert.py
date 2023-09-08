fafa6e5f878ce981027c5f1a28ce453ec65eef3b
Ars Vladimirov <grokhael@gmail.com>
2023-09-08 19:09:36 +0600

    init setting

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,24 @@
-import supervisely as sly
-import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
+# https://www.kaggle.com/datasets/chrisfilo/fruit-recognition
 
+import glob
+import os
+import shutil
+from urllib.parse import unquote, urlparse
+
+import numpy as np
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.io.fs import (
+    file_exists,
+    get_file_name,
+    get_file_name_with_ext,
+    get_file_size,
+)
 from tqdm import tqdm
+
+import src.settings as s
+from dataset_tools.convert import unpack_if_archive
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -52,7 +64,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -60,21 +73,140 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    # project_name = "Fruit Recognition"
+    images_path = "/mnt/d/datasetninja-raw/fruit-recognition"
+    ds_name = "ds"
+    batch_size = 30
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    contain_subfolders = ["Apple", "Guava", "Kiwi"]
+    duplicates_folders = [
+        "Total Number of Apples",
+        "Guava total",
+        "guava total final",
+        "Total Number of Kiwi fruit",
+    ]
 
-    # ... some code here ...
+    def create_ann(image_path, fruit_folder_):
+        labels = []
+        tags = []
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # return project
+        fruit = sly.Tag(meta.get_tag_meta(fruit_folder_.lower()))
+        tags.append(fruit)
 
+        possible_subfolder = image_path.split("/")[-2]
+        if possible_subfolder != fruit_folder_:
+            subfolder_tag = sly.Tag(tag_subfolder_data, value=possible_subfolder)
+            tags.append(subfolder_tag)
 
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+    tag_apple = sly.TagMeta("apple", sly.TagValueType.NONE)
+    tag_banana = sly.TagMeta("banana", sly.TagValueType.NONE)
+    tag_carambola = sly.TagMeta("carambola", sly.TagValueType.NONE)
+    tag_guava = sly.TagMeta("guava", sly.TagValueType.NONE)
+    tag_kiwi = sly.TagMeta("kiwi", sly.TagValueType.NONE)
+    tag_mango = sly.TagMeta("mango", sly.TagValueType.NONE)
+    tag_muskmelon = sly.TagMeta("muskmelon", sly.TagValueType.NONE)
+    tag_orange = sly.TagMeta("orange", sly.TagValueType.NONE)
+    tag_peach = sly.TagMeta("peach", sly.TagValueType.NONE)
+    tag_pear = sly.TagMeta("pear", sly.TagValueType.NONE)
+    tag_persimmon = sly.TagMeta("persimmon", sly.TagValueType.NONE)
+    tag_pitaya = sly.TagMeta("pitaya", sly.TagValueType.NONE)
+    tag_plum = sly.TagMeta("plum", sly.TagValueType.NONE)
+    tag_pomegranate = sly.TagMeta("pomegranate", sly.TagValueType.NONE)
+    tag_tomatoes = sly.TagMeta("tomatoes", sly.TagValueType.NONE)
+    tag_subfolder_data = sly.TagMeta("sub-category", sly.TagValueType.ANY_STRING)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        tag_metas=[
+            tag_apple,
+            tag_banana,
+            tag_carambola,
+            tag_guava,
+            tag_kiwi,
+            tag_mango,
+            tag_muskmelon,
+            tag_orange,
+            tag_peach,
+            tag_pear,
+            tag_persimmon,
+            tag_pitaya,
+            tag_plum,
+            tag_pomegranate,
+            tag_tomatoes,
+            tag_subfolder_data,
+        ],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    for fruit_folder in os.listdir(images_path):
+        curr_images_path = os.path.join(images_path, fruit_folder)
+        if fruit_folder in contain_subfolders:
+            for subfolder in os.listdir(curr_images_path):
+                if subfolder in duplicates_folders:
+                    continue
+                sub_images_path = os.path.join(curr_images_path, subfolder)
+                images_names = os.listdir(sub_images_path)
+
+                progress = sly.Progress(
+                    "Create dataset {}, add {} data".format(ds_name, subfolder), len(images_names)
+                )
+
+                for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+                    images_pathes_batch = [
+                        os.path.join(sub_images_path, image_path) for image_path in img_names_batch
+                    ]
+
+                    if fruit_folder == "Apple":
+                        prefix = subfolder.split(" ")[1]
+                        if prefix != "Number":
+                            img_names_batch = [
+                                prefix + "_" + im_name for im_name in img_names_batch
+                            ]
+
+                    img_infos = api.image.upload_paths(
+                        dataset.id, img_names_batch, images_pathes_batch
+                    )
+                    img_ids = [im_info.id for im_info in img_infos]
+
+                    anns_batch = [
+                        create_ann(image_path, fruit_folder) for image_path in images_pathes_batch
+                    ]
+                    api.annotation.upload_anns(img_ids, anns_batch)
+
+                    progress.iters_done_report(len(img_names_batch))
+
+        else:
+            images_names = os.listdir(curr_images_path)
+
+            progress = sly.Progress(
+                "Create dataset {}, add {} data".format(ds_name, fruit_folder), len(images_names)
+            )
+
+            for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+                images_pathes_batch = [
+                    os.path.join(curr_images_path, image_path) for image_path in img_names_batch
+                ]
+
+                img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+                img_ids = [im_info.id for im_info in img_infos]
+
+                anns_batch = [
+                    create_ann(image_path, fruit_folder) for image_path in images_pathes_batch
+                ]
+                api.annotation.upload_anns(img_ids, anns_batch)
+
+                progress.iters_done_report(len(img_names_batch))
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,39 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Fruit Recognition"
+PROJECT_NAME_FULL: str = "Fruit Recognition"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC_BY_4_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Food(), Industry.Retail()]
+CATEGORY: Category = Category.Food()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.Classification()]
+ANNOTATION_TYPES: List[AnnotationType] = []
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2018-07-12"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://zenodo.org/record/1310165"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 3376532
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/fruit-recognition"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://zenodo.org/record/1310165/files/Fruit%20-Database.rar?download=1"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,18 +53,56 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[
+    Union[str, List[str], Dict[str, str]]
+] = "https://www.hindawi.com/journals/cin/2022/6538117/"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
 REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://zenodo.org/record/1310165/export/hx"
+AUTHORS: Optional[List[str]] = [
+    "Dostdar Hussain",
+    "Israr Hussain",
+    "Muhammad Ismail",
+    "Amerah Alabrah",
+    "Syed Sajid Ullah",
+    "Hayat Mansoor Alaghbari",
+]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Karakoram International University, Pakistan",
+    "King Saud University, Saudi Arabia",
+    "University of Agder, Norway",
+    "Taiz University, Yemen",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://www.kiu.edu.pk/",
+    "https://ksu.edu.sa/en/",
+    "https://www.uia.no/en",
+    "https://taiz.edu.ye/en/default.ASPX",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "fruit categories": [
+        "apple",
+        "banana",
+        "carambola",
+        "guava",
+        "kiwi",
+        "mango",
+        "muskmelon",
+        "orange",
+        "peach",
+        "pear",
+        "persimmon",
+        "pitaya",
+        "plum",
+        "pomegranate",
+        "tomatoes",
+    ],
+    "__POSTTEXT__": "Additionally, some fruit images contain information about different fruit kinds in ***sub-category***",
+}
 TAGS: Optional[List[str]] = None
 
 
